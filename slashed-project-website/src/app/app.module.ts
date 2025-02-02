458917d72eb9c2457dfb2187493775ac4742aac2
Gascor <lucas-dasilvaferreira@hotmail.com>
2025-02-02 22:30:07 +0100

    test

--- slashed-project-website/src/app/app.module.ts
+++ slashed-project-website/src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
-import { VideoBackgroundComponent } from './video-background/video-background.component'; // Adjust the path as necessary
+import { VideoBackgroundComponent } from '../assets/cinematicTest.mp4'; // Adjust the path as necessary
 
 @NgModule({
   declarations: [
