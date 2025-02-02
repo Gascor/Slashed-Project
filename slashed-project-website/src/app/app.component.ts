f01a7d9a3f97427cb145652e6c5e2a64253de7b7
Gascor <lucas-dasilvaferreira@hotmail.com>
2025-02-02 23:19:35 +0100

    test

--- slashed-project-website/src/app/app.component.ts
+++ slashed-project-website/src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
-import { VideoBackgroundComponent } from './video-background/video-background.component'; // Chemin corrigé
+import { HeaderComponent } from './header/header.component';
+import { VideoBackgroundComponent } from './video-background/video-background.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
   standalone: true,
-  imports: [VideoBackgroundComponent] // Ajoutez cette ligne
+  imports: [HeaderComponent, VideoBackgroundComponent]
 })
 export class AppComponent {
   title = 'Slashed Project';
