cdb50a71cbcc95c4af0bdc6911261849077ba96c
Gascor <lucas-dasilvaferreira@hotmail.com>
2025-02-02 22:48:32 +0100

    test

--- slashed-project-website/src/app/app.module.ts
+++ slashed-project-website/src/app/app.module.ts
@@ -1,15 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
-import { VideoBackgroundComponent } from './video-background.component'; // Chemin corrigé
+import { VideoBackgroundComponent } from './video-background/video-background.component'; // Chemin corrigé
 
 @NgModule({
-  declarations: [
+  imports: [
+    BrowserModule,
     AppComponent,
     VideoBackgroundComponent
-  ],
-  imports: [
-    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- slashed-project-website/src/app/video-background/video-background.component.ts
+++ slashed-project-website/src/app/video-background/video-background.component.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-video-background',
   templateUrl: './video-background.component.html',
-  styleUrls: ['./video-background.component.css']
+  styleUrls: ['./video-background.component.css'],
+  standalone: true
 })
 export class VideoBackgroundComponent { }
