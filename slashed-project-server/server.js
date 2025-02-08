c644ab1ee8425a734d36d45bb1abb9db4edafc2f
Gascor <lucas-dasilvaferreira@hotmail.com>
2025-02-08 20:15:53 +0100

    test

--- slashed-project-server/server.js
+++ slashed-project-server/server.js
@@ -7,13 +7,23 @@ const pool = new Pool({
   user: 'slashed_user',
   host: 'db', // Utilisez le nom du service Docker pour l'hôte
   database: 'slashed_project',
-  password: 'your_password',
+  password: 'slashed_user',
   port: 5432,
 });
 
 app.get('/news', async (req, res) => {
   try {
     const result = await pool.query('SELECT * FROM news ORDER BY created_at DESC');
+    res.json(result.rows);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+app.get('/test-db', async (req, res) => {
+  try {
+    const result = await pool.query('SELECT NOW()');
     res.json(result.rows);
   } catch (err) {
     console.error(err);
