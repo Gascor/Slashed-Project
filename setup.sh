81e76e71a3ed6708ad2d173dab15e7cf7dc5b2d6
Gascor <lucas-dasilvaferreira@hotmail.com>
2025-02-08 19:52:01 +0100

    test

--- setup.sh
+++ setup.sh
@@ -60,8 +60,8 @@ uninstall_project() {
     echo "Désinstallation du projet..."
     cd "$DEPLOY_DIR"
     sudo docker-compose -f docker-compose.yml down
-    cd ../"$API_DB_DIR"
     sudo docker rmi $(sudo docker images 'slashed-project_web' -a -q)
+    cd ../"$API_DB_DIR"
     sudo docker rmi $(sudo docker images 'slashed-project-server_db_1' -a -q)
     sudo docker rmi $(sudo docker images 'slashed-project-server_server_1' -a -q)
     sudo docker volume prune -f
